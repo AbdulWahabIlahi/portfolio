2506213a0287955a94ab6062b1f67079ceefb235
AbdulWahabIlahi <nemesisdragon18@gmail.com>
2025-05-02 10:43:08 +0530

    Re-add landingPage folder with all files

---
+++ landingPage/projects.js
@@ -0,0 +1,44 @@
+export const projectsData = [
+    {
+      name: "Shadow of the Leaf",
+      director: "Kakashi Hatake",
+      location: "Konohagakure",
+    },
+    {
+      name: "Rasengan Rebirth",
+      director: "Naruto Uzumaki",
+      location: "Hidden Leaf Village",
+    },
+    {
+      name: "Sasuke's Redemption",
+      director: "Sasuke Uchiha",
+      location: "Land of Fire",
+    },
+    {
+      name: "Sakura's Resolve",
+      director: "Sakura Haruno",
+      location: "Hidden Leaf Hospital",
+    },
+    {
+      name: "Sandstorm Legacy",
+      director: "Gaara",
+      location: "Sunagakure",
+    },
+    {
+      name: "Byakugan Awakening",
+      director: "Hinata Hyuga",
+      location: "Hyuga Clan District",
+    },
+    {
+      name: "Beast Within",
+      director: "Killer Bee",
+      location: "Kumogakure",
+    },
+    {
+      name: "Eyes of the Moon",
+      director: "Madara Uchiha",
+      location: "Valley of the End",
+    }
+];
+
+
